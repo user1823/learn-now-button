8d4bbdedf80ef8ffc45c748e2b76ae517c0c15a3
user1823 <92206575+user1823@users.noreply.github.com>
2026-03-21 14:05:11 +0530

    Remove "Grade now" button from browser menu
    I always confuse it with the Anki's built-in Grade now button.

--- card_management/browser_menus.py
+++ card_management/browser_menus.py
@@ -2,13 +2,11 @@ from aqt import gui_hooks
 from aqt.browser import Browser
 
 from .config import config
-from .grade_now import add_grade_now_buttons
 from .learn_now import add_learn_now_button
 
 
 def on_browser_menus_did_init(self: Browser) -> None:
     add_learn_now_button(self, shortcut=config.learn_shortcut)
-    add_grade_now_buttons(self, config=config)
 
 
 def init() -> None:
